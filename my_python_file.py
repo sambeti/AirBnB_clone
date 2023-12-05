e2ca058525e3b69f20e7e9de38175dbf37387eaa
Samuel Karmole <sambeti2002@gmail.com>
2023-12-05 10:57:32 -0800

    Code that checks pycodestyle compliant

---
+++ my_python_file.py
@@ -0,0 +1,11 @@
+def <link>factorial</link>(n):
+    """Calculate the factorial of a number."""
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
